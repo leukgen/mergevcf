8c3c6c2c998ec5785042394983320c272cee2f4d
Juan Esteban Arango <juanes.ao@gmail.com>
2023-02-16 14:11:39 -0500

    🐛 hot fix to skip malformed records

--- mergesvvcf/mergedfile.py
+++ mergesvvcf/mergedfile.py
@@ -271,6 +271,7 @@ def merge(
         "Strand orientation of the adjacency in BEDPE format (DEL:+-, DUP:-+, INV:++/--)",
     )
 
+    outfile = open(outfile,'w')
     print(outvcf.header, end="", file=outfile)
 
     for variant in sorted(calldict, key=operator.itemgetter(0)):
@@ -307,9 +308,12 @@ def merge(
             extend1, extend2 = avgloc1.__right__, avgloc2.__right__
             ref, alt = bkptRefAltFromPair(avgloc1, avgloc2)
             vcfrec = outvcf.new_record()
-            vcfrec.contig = avgloc1.chrom
-            vcfrec.pos = avgloc1.pos
-            vcfrec.ref = ref
+            try:
+                vcfrec.contig = avgloc1.chrom
+                vcfrec.pos = avgloc1.pos
+                vcfrec.ref = ref
+            except:
+                continue
             vcfrec.alts = [alt]
             vcfrec.filter.add(filterstring)
             # for key, val in sorted(infoString(callers, make_info_dict(callers, records, medianPos1, medianPos2)).items()):
